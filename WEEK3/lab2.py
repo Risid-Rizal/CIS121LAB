b22cf278147e34e680183408ba9d231f4253464b
Risid-Rizal <risid.rizal@mnsu.edu>
2025-09-12 09:10:01 -0500

    remove folder

--- WEEK3/lab2.py
+++ WEEK3/lab2.py
@@ -1,1 +1,13 @@
 #created by Risid Rizal 
+
+if my_var % 2 == 1:
+    if my_var ** 3 != 27:
+        my_var = my_var + 4   
+    else:
+        my_var /= 1.5         
+else:
+    if my_var <= 10:
+        my_var *= 2           
+    else:
+        my_var == 2          
+print(my_var)
